ff555300abe42b70b905a28e4efb0ffe72760ffb
Stephen Sun <stephensun.me@gmail.com>
2018-08-09 19:10:07 -0500

    added home view and routing

--- public/index.js
+++ public/index.js
@@ -1,5 +1,16 @@
 var youtubeClone = angular.module('YoutubeClone', []);
 
+youtubeClone.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'home.html',
+      controller: 'VideoController'
+    })
+    if(window.history && window.history.pushState) {
+      $locationProvider.html5Mode(true);
+    }
+}]);
+
 youtubeClone.controller('VideoController', function($scope) {
   console.log('This is the Video Controller');
 });
