a9ca9d8b0f9e4a03699faa3961eddb8c4864e516
Stephen Sun <stephensun.me@gmail.com>
2018-08-09 22:43:16 -0500

    added server side routes and controllers, making request on server side

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
-var youtubeClone = angular.module('YoutubeClone', []);
+var youtubeClone = angular.module('YoutubeClone', ['ngRoute']);
 
 youtubeClone.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'home.html',
+      templateUrl: 'views/home.html',
       controller: 'VideoController'
     })
     if(window.history && window.history.pushState) {
@@ -11,6 +11,34 @@ youtubeClone.config(['$routeProvider', '$locationProvider', function($routeProvi
     }
 }]);
 
-youtubeClone.controller('VideoController', function($scope) {
+youtubeClone.controller('VideoController', function($scope, $http) {
   console.log('This is the Video Controller');
+
+  // $scope.getAllVideos = function() {
+  //   $http.get(
+  //     'https://www.googleapis.com/youtube/v3/channels', {
+  //     params: {
+  //       'id': 'UCKPbQfwD_EoQtCKetDVDUmQ',
+  //       'part': 'snippet,contentDetails,statistics',
+  //       'key': 'AIzaSyAWta3FX760zH3ny9Z9C06YxutMlushPjo'
+  //     }
+  // })
+  //   .then(res => {
+  //     console.log(res);
+  //     $scope.videos = res.data.items[0];
+  //     console.log($scope.videos);
+  //   })
+  // }
+
+  $scope.getChannelData = function() {
+    // $http.get('/api/key').then(res => {
+    //   $scope.key = res.data;
+    //   console.log($scope.key);
+    // });
+
+    $http.get('/api/channel').then(res => {
+      $scope.channel = res.data;
+      console.log(res);
+    });
+  }
 });
