318a5861817ebd85c3e9acb0514a8824295c5666
Stephen Sun <stephensun.me@gmail.com>
2018-08-11 12:03:44 -0500

    added npm request and get calls are working

--- server/controllers/channel.controller.js
+++ server/controllers/channel.controller.js
@@ -1,48 +1,45 @@
 var request = require('request');
 
 exports.channel = function(req, res) {
-  request(
-    'https://www.googleapis.com/youtube/v3/channels', {
-      data: {
-        'id': 'UCKPbQfwD_EoQtCKetDVDUmQ',
-        'part': 'snippet,contentDetails,statistics',
-        'key': process.env.YOUTUBE_DATA_KEY
-      }
-    }, function(error, response, body) {
-      console.log('error:', error); // Print the error if one occurred and handle it
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      res.send(body);
-    }
-  );
+  const options1 = {
+    method: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/channels',
+    qs: {
+      id: 'UCKPbQfwD_EoQtCKetDVDUmQ',
+      part: 'snippet,contentDetails,statistics',
+      key: process.env.YOUTUBE_DATA_KEY
+    },
+    json: true
+  };
 
-  // request(
-  //   'https://www.googleapis.com/youtube/v3/search', {
-  //     data: {
-  //     channelId: 'UCKPbQfwD_EoQtCKetDVDUmQ',
-  //     part: 'snippet',
-  //     key: process.env.YOUTUBE_DATA_KEY
-  //   },
-  //     function(error, response, body) {
-  //       console.log('error:', error); // Print the error if one occurred and handle it
-  //       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //       res.send(body);
-  //   }
-  //   });
+  const options2 = {
+    method: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    qs: {
+      channelId: 'UCKPbQfwD_EoQtCKetDVDUmQ',
+      part: 'snippet',
+      key: process.env.YOUTUBE_DATA_KEY
+    },
+    json: true
+  };
 
-  // request(
-  //   'https://www.googleapis.com/youtube/v3/activities', {
-  //     'data': {
-  //       'channelId': 'UCKPbQfwD_EoQtCKetDVDUmQ',
-  //       'maxResults': '25',
-  //       'part': 'snippet,contentDetails',
-  //       'key': process.env.YOUTUBE_DATA_KEY
-  //     }
-  //   }, function(error, response, body) {
-  //     console.log('error:', error); // Print the error if one occurred and handle it
-  //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //     res.send(body);
-  //   }
-  // );
+  const options3 = {
+    method: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/activities',
+    qs: {
+      'channelId': 'UCKPbQfwD_EoQtCKetDVDUmQ',
+      'maxResults': '25',
+      'part': 'snippet,contentDetails',
+      'key': process.env.YOUTUBE_DATA_KEY
+    },
+    json: true
+  };
+
+  request(options3, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(body);
+  });
 }
 
 
